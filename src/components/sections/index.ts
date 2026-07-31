36eff899b2332135cf6131a534744a4d2ec13257
MUZAMMIL-JAVED <113161533+MUZAMMIL-JAVED@users.noreply.github.com>
2026-07-31 23:13:54 +0500

    Update

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -2,5 +2,6 @@ export { HeroSection } from "./HeroSection";
 export { CapabilitiesSection } from "./CapabilitiesSection";
 export { TeamSection } from "./TeamSection";
 export { DemoSection } from "./DemoSection";
+export { WorkSection } from "./WorkSection";
 export { ProcessSection } from "./ProcessSection";
 export { ContactSection } from "./ContactSection";
